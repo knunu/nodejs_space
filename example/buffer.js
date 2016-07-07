0d283bb78909c5a85d68fad74e9d0a148c343f3d
Knunu <burette@hanyang.ac.kr>
2016-07-07 14:41:07 +0900

    example update

--- example/buffer.js
+++ example/buffer.js
@@ -2,14 +2,15 @@
  * Created by Knunu on 2016. 7. 4..
  */
 
-var buf = new Buffer(10);
+var buf = new Buffer(26);
 var buf2;
 var buf3 = new Buffer("Simple buffer", "utf-8");
 
+// write
 var len = buf.write("Simply Easy Learning");
 console.log("Octets written : " + len);
 
-buf2 = new Buffer(26);
+// toString
 for (var i = 0; i < 26; i++) {
     buf[i] = i + 97;
 }
@@ -18,5 +19,28 @@ console.log(buf.toString('ascii',0,5));
 console.log(buf.toString('utf8',0,10));
 console.log(buf.toString(undefined,0,15));
 
+// json
 var json = buf.toJSON(buf3);
 console.log(json);
+
+// concat
+buf2 = new Buffer(' Tutorials');
+var concatBuf = Buffer.concat([buf3, buf2]);
+console.log("concatenated buffer content : " + concatBuf.toString());
+
+// compare
+buf = new Buffer('ABCC');
+buf2 = new Buffer('ABCD');
+var result = buf.compare(buf2);
+
+if (result < 0) {
+    console.log(buf + " comes before " + buf2);
+} else if (result == 0) {
+    console.log(buf + " is same as " + buf2);
+} else {
+    console.log(buf + " comes after " + buf2);
+}
+
+// length
+console.log("buffer length : " + buf2.length);
+
